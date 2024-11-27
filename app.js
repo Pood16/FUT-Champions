2fc924cf19586b616693ffd75d2924fb9e8d1989
lahcen <lahcenwirghanec11@gmail.com>
2024-11-27 00:54:58 +0000

    FC-3 fonction d'affichage des cartes

--- app.js
+++ app.js
@@ -69,12 +69,78 @@ function createCard(e){
     players.push(playerInformation);
     localStorage.setItem("players", JSON.stringify(players));
     afficherCards();
+    e.target.reset();
 }
 function afficherCards(){
     let cardsContainer = document.getElementById("cards");
     cardsContainer.innerHTML = '';
-    players.forEach((player, index)=>{
-        
+    players.forEach(player =>{
+        const playerContainer = document.createElement("div");
+        playerContainer.className = "card-container";
+        let partieNonCommun;
+        if (player.position === "GK"){
+            partieNonCommun = `
+                <div class="horizental-stats">
+                  <p>DIV</p>
+                  <p>${player.diving}</p>
+                  <p>HAN</p>
+                  <p>${player.handling}</p>
+                  <p>CIK</p>
+                  <p>${player.kicking}</p>
+                </div>
+                <div class="bottom-stats">
+                  <div class="vertical-stats">
+                    <p>REF</p>
+                    <p>SPE</p>
+                    <p>POS</p>
+                  </div>
+                  <div class="vertical-stats">  
+                    <p>${player.reflexes}</p>
+                    <p>${player.speed}</p>
+                    <p>${player.positioning}</p>
+                  </div>
+                </div>  `
+        }else{
+            partieNonCommun = 
+                `
+                <div class="horizental-stats">
+                  <p>PAC</p>
+                  <p>${player.pace}</p>
+                  <p>SHO</p>
+                  <p>${player.shooting}</p>
+                  <p>PAS</p>
+                  <p>${player.passing}</p>
+                </div>
+                <div class="bottom-stats">
+                  <div class="vertical-stats">
+                    <p>DRI</p>
+                    <p>DEF</p>
+                    <p>PHY</p>
+                  </div>
+                  <div class="vertical-stats">  
+                    <p>${player.dribbling}</p>
+                    <p>${player.defending}</p>
+                    <p>${player.physical}</p>
+                  </div>
+                </div>
+      `
+        }
+        playerContainer.innerHTML = `
+            <img src="img/bg/card.webp" alt="card background" width="100%"/>
+                <div class="position-rate">
+                  <p>${player.rating}</p>
+                  <p>${player.position}</p>
+                </div>
+                <img src="${player.photo}" alt="${player.name}" class="player-profile">
+                <h3 class="player-name">${player.name}</h3>
+                <div class="footer-card">
+                  <img src="${player.flag}" alt="country flag" class="country-flag" width="20px">
+                  <img src="${player.club}" alt="club flag" class="club-flag" width="20px">
+                </div>
+                ${partieNonCommun}
+    
+        `;
+        cardsContainer.appendChild(playerContainer);
     })
 }
 
